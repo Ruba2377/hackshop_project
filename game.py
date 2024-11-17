0a68490c01876177ac6ea6e79f231e7c7d652f48
Ruba2377 <rubalakshmi2377@gmail.com>
2024-11-17 23:12:42 +0530

    created game

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import random
 st.title("Simple Number Guessing Game")
-st.write("I have selected a random number between 1 and 100. Try to guess it")
-random_number = random.randint(1, 100)
-guess = st.number_input("Enter your guess:", min_value=1, max_value=100, step=1)
+st.write("I have selected a random number between 1 and 10. Try to guess it")
+random_number = random.randint(1, 10)
+guess = st.number_input("Enter your guess:", min_value=1, max_value=10, step=1)
 if st.button("Submit Guess"):
     if guess < random_number:
         st.write("Too low! Try again.")
