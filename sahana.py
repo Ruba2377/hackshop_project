0dfc3582847286d4875e4e59db8a76a91894a7a0
Ruba2377 <rubalakshmi2377@gmail.com>
2024-11-18 10:41:20 +0530

    created sahana

---
+++ sahana.py
@@ -0,0 +1,14 @@
+import random
+number=random.randint(1,10)
+for x in range(3):
+    guess=int(input("Take a guess from 1 to 10:"))
+if guess==number:
+    print("you won!!")
+    break
+elif guess>number:
+    print("Try lower!!")
+elif guess<number:
+    print("Try Higher!!")
+else:
+    print("You Lost!!!")
+    break
